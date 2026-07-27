655a23b9778c31394a23438d15f7f1f78fe55a25
arnyinson <arnyinson2516@email.com>
2026-07-27 10:13:00 +0800

    Update API base URL to deployed backend

--- constants/api.ts
+++ constants/api.ts
@@ -1,7 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
-const API_BASE_URL = "http://192.168.254.107:5000/api";
+const API_BASE_URL = "https://nutrifit-backend-api-t21p.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
